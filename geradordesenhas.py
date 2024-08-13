440adeab1bb6cb556d6915ac993742721ff33cdf
Luigi Del Vecchio (Cadica) <luigi.dv@hotmail.com>
2024-08-13 17:06:28 -0300

    Update geradordesenhas.py
    Agora tambem verifica se tem pelo menos 2 numeros na senha

--- geradordesenhas.py
+++ geradordesenhas.py
@@ -10,8 +10,9 @@ while len(senhas_geradas)< 5:
 
     upper_count = sum(1 for x in senha_forte if x.isupper())
     special_count = sum(1 for x in senha_forte if x in caracteres_especiais)
+    numbers_count = sum(1 for x in senha_forte if x.isdigit())
 
-    if upper_count >= 2 and special_count >= 2:
+    if upper_count >= 2 and special_count >= 2 and numbers_count >= 2:
         senhas_geradas.append(senha_forte)
 senhas = ' / '.join(senhas_geradas)
 print("/",senhas,"/")
