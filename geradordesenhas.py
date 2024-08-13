1719dd79383e0966180f8d22ae23e5ba698c8283
Luigi Del Vecchio (Cadica) <luigi.dv@hotmail.com>
2024-08-13 16:06:55 -0300

    Add files via upload
    Codigo final

---
+++ geradordesenhas.py
@@ -0,0 +1,17 @@
+import secrets
+import string
+
+caracteres_especiais = ''.join(x for x in string.punctuation if x in '@#!?')
+letras = string.ascii_letters + string.digits + caracteres_especiais
+senhas_geradas = []
+
+while len(senhas_geradas)< 5:
+    senha_forte = ''.join(secrets.choice(letras) for x in range(13))
+
+    upper_count = sum(1 for x in senha_forte if x.isupper())
+    special_count = sum(1 for x in senha_forte if x in caracteres_especiais)
+
+    if upper_count >= 2 and special_count >= 2:
+        senhas_geradas.append(senha_forte)
+senhas = ' / '.join(senhas_geradas)
+print("/",senhas,"/")
